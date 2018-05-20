624d76da9330aed7917c513d6656786fa6d24c62
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-19 19:46:47 -0600

    Initial react component structure

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
+import Nav from '../Nav';
+import RobotContainer from '../RobotContainer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: []
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +19,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Nav />
+          <RobotContainer />
+        </main>
       </div>
     );
   }
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = (props) => {
+  return (
+    <div className='App__Nav--nav-wrapper'>
+      <nav>
+        <h1>I'm the nav!</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav;
