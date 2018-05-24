26c675a33671795d934900538cbf87b80c65108d
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-24 12:58:43 -0600

    add methods to app component for api calls

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
+import api from '../../api';
 import RobotContainer from '../RobotContainer';
 import './App.css';
 
@@ -8,11 +9,53 @@ class App extends Component {
     super(props);
     this.state = {
       robots: [],
-      robotField: false
+      robotField: false,
+      errorStatus: ''
     };
   }
 
+  async componentDidMount() {
+    const robots = await api.getAllRobots();
+    this.setState({ robots });
+  }
+
+  addRobot = async (newRobot) => {
+    try {
+      const robot = api.postRobot(robot);
+    } catch (error) {
+      this.setState({ errorStatus: error });
+    }
+  }
+
+  editRobot = async (changedRobot) => {
+    try {
+      const updatedRobot = await api.updateRobot(changedRobot);
+      const robots = this.state.robots.map(robot => {
+        if (robot.id === changedRobot.id) {
+          return updatedRobot;
+        } else {
+          return robot
+        }
+      });
+      this.setState({ robots });
+    } catch (error) {
+      this.setState({ errorStatus: error });
+    }
+  }
+
+  removeRobot = async (id) => {
+    try {
+      await api.deleteRobot(id);
+      const robots = this.state.robots.filter(robot => robot.id !== id);
+      this.setState({ robots });
+    } catch (error) {
+      this.setState({ errorStatus: error });
+    }
+  }
+
   render() {
+    const { robots, robotField } = this.state;
+    const { addRobot, editRobot, removeRobot } = this;
     return (
       <div className="App">
         <header className="App-header">
@@ -21,7 +64,9 @@ class App extends Component {
         </header>
         <main>
           <RobotContainer 
-            robots={this.state.robots}
+            robots={robots}
+            editRobot={editRobot}
+            removeRobot={removeRobot}
           />
         </main>
       </div>
