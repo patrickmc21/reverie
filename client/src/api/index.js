0bf7755f3aac03ee46f8e47cce37f0f1c2f7e1bc
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-24 12:16:44 -0600

    import calls into index of api

---
+++ client/src/api/index.js
@@ -0,0 +1,9 @@
+import deleteRobot from './deleteRobot';
+import getAllRobots from './getAllRobots';
+import getSingleRobot from './getSingleRobot';
+import postRobot from './postRobot';
+import updateRobot from './updateRobot';
+
+const api = {deleteRobot, getAllRobots, getSingleRobot, postRobot, updateRobot};
+
+export default api;
